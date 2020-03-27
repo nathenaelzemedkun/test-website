7080b50173bb1289fea86913bc0272eda5d4209a
Nathenael Zemdkun <nathenaelzemdkun@gmail.com>
2020-03-26 17:59:28 -0700

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: '<h1>My First Angular App</h1>',
+  
+})
+export class AppComponent implements OnInit { }
