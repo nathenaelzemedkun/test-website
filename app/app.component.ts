fe208ded1e9875d1602a32a16ddbfeeb9820bd7b
Nathenael Zemdkun <nathenaelzemdkun@gmail.com>
2020-03-27 16:12:31 -0700

    templaturl changed to tamplate

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: '<h1>My First Angular GITHUB App</h1>',
+  template: '<h1>My First Angular GITHUB App</h1>',
   
 })
 export class AppComponent z { }
